a639616024e93a7c53a8ed3851b9de7c02343d9e
John Lonetree <jlonetree@gmail.com>
2020-08-09 22:02:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def base_hash
   monopoly = {
-    :railroad => []
+    :railroads => []
   }
 end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroad => {
+    :railroads => {
       :pieces => 4,
       :rent_in_dollars => [],
       :names => []
