dfa19eb935116ca43d462842b53b1532ce9d3ac2
John Lonetree <jlonetree@gmail.com>
2020-08-09 22:04:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,15 +8,20 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => [],
-      :names => []
+      :rent_in_dollars => {},
+      :names => {}
     }
   }
 end
 
 def monopoly_with_third_tier
-  ;
-
+  monopoly = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {},
+      :names => {}
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
